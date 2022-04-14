4cb9feafa9ab23dbcd43729c9789f7a7f3b504db
Corentin503 <102851724+Corentin503Dev@users.noreply.github.com>
2022-04-14 22:03:57 +0200

    Add files via upload

---
+++ src/soups/Main.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace soups;
+
+use pocketmine\event\Listener;
+use pocketmine\event\player\PlayerInteractEvent;
+use pocketmine\event\player\PlayerItemUseEvent;
+use pocketmine\plugin\PluginBase;
+
+class Main extends PluginBase  implements Listener
+{
+    public function onEnable(): void
+    {
+        $this->getLogger()->info("Plugin Soups bien activé !");
+        $this->getServer()->getPluginManager()->registerEvents($this, $this);
+        @mkdir($this->getDataFolder());
+        $this->saveDefaultConfig();
+    }
+    public function Soups(PlayerItemUseEvent $event){
+        $player= $event->getPlayer();
+        if ($player->getInventory()->getItemInHand()->getId() == $this->getConfig()->get("id")){
+            $item = $event->getItem();
+            $item->setCount("1");
+            $player->setHealth($player->getHealth() + $this->getConfig()->get("heal_add"));
+            $player->getInventory()->removeItem($item);
+        }
+    }
+}
