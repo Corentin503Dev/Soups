20f9d96b16bdb784c6d9d278213b7ae6c985f87e
Corentin503 <102851724+Corentin503Dev@users.noreply.github.com>
2022-04-27 09:45:27 +0200

    Add Config.yml

--- src/soups/Main.php
+++ src/soups/Main.php
@@ -3,7 +3,6 @@
 namespace soups;
 
 use pocketmine\event\Listener;
-use pocketmine\event\player\PlayerInteractEvent;
 use pocketmine\event\player\PlayerItemUseEvent;
 use pocketmine\plugin\PluginBase;
 
@@ -16,13 +15,18 @@ class Main extends PluginBase  implements Listener
         @mkdir($this->getDataFolder());
         $this->saveDefaultConfig();
     }
-    public function Soups(PlayerItemUseEvent $event){
-        $player= $event->getPlayer();
-        if ($player->getInventory()->getItemInHand()->getId() == $this->getConfig()->get("id")){
-            $item = $event->getItem();
-            $item->setCount("1");
-            $player->setHealth($player->getHealth() + $this->getConfig()->get("heal_add"));
-            $player->getInventory()->removeItem($item);
+    public function Soups(PlayerItemUseEvent $event)
+    {
+        $player = $event->getPlayer();
+        if ($player->getInventory()->getItemInHand()->getId() == $this->getConfig()->get("id")) {
+            if ($player->getHealth() == $player->getMaxHealth()) {
+                $event->cancel();
+            } else {
+                $item = $event->getItem();
+                $item->setCount("1");
+                $player->setHealth($player->getHealth() + $this->getConfig()->get("heal_add"));
+                $player->getInventory()->removeItem($item);
+            }
         }
     }
 }
